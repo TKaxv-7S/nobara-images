4c0064dff17e64094518ac03f87e125783b5a2c9
GloriousEggroll <gloriouseggroll@gmail.com>
2026-08-30 10:17:47 -0600

    update 44 build.sh

--- build.sh
+++ build.sh
@@ -1,43 +1,28 @@
 #!/bin/bash
 
-VERSION="43"
+VERSION="44"
 mkdir complete
 
-livemedia-creator --ks flat-nobara-live-official-43.ks --no-virt --resultdir ./release --project Nobara --make-iso --volid Nobara-43 --iso-only --iso-name Nobara-43-Official-$(date +%F).iso --releasever 43 --macboot
+livemedia-creator --ks nv-flat-nobara-live-official-44.ks --no-virt --resultdir ./release --project Nobara --make-iso --volid Nobara-44 --iso-only --iso-name Nobara-44-Official-NV-$(date +%F).iso --releasever 43 --macboot --extra-boot-args "modules_load=nvidia"
 mv ./release/*.iso ./complete
 rm -Rf ./release
 
-livemedia-creator --ks flat-nobara-live-gnome-43.ks --no-virt --resultdir ./release --project Nobara --make-iso --volid Nobara-43 --iso-only --iso-name Nobara-43-GNOME-$(date +%F).iso --releasever 43 --macboot
+livemedia-creator --ks nv-flat-nobara-live-gnome-44.ks --no-virt --resultdir ./release --project Nobara --make-iso --volid Nobara-44 --iso-only --iso-name Nobara-44-GNOME-NV-$(date +%F).iso --releasever 43 --macboot --extra-boot-args "modules_load=nvidia"
 mv ./release/*.iso ./complete
 rm -Rf ./release
 
-livemedia-creator --ks flat-nobara-live-kde-43.ks --no-virt --resultdir ./release --project Nobara --make-iso --volid Nobara-43 --iso-only --iso-name Nobara-43-KDE-$(date +%F).iso --releasever 43 --macboot
+livemedia-creator --ks nv-flat-nobara-live-kde-44.ks --no-virt --resultdir ./release --project Nobara --make-iso --volid Nobara-44 --iso-only --iso-name Nobara-44-KDE-NV-$(date +%F).iso --releasever 43 --macboot --extra-boot-args "modules_load=nvidia"
 mv ./release/*.iso ./complete
 rm -Rf ./release
 
-livemedia-creator --ks flat-nobara-live-steam-htpc-43.ks --no-virt --resultdir ./release --project Nobara --make-iso --volid Nobara-43 --iso-only --iso-name Nobara-43-Steam-HTPC-$(date +%F).iso --releasever 43 --macboot
+livemedia-creator --ks nv-flat-nobara-live-steam-htpc-44.ks --no-virt --resultdir ./release --project Nobara --make-iso --volid Nobara-44 --iso-only --iso-name Nobara-44-Steam-HTPC-NV-$(date +%F).iso --releasever 43 --macboot --extra-boot-args "modules_load=nvidia" --lorax-templates ./lorax-custom
 mv ./release/*.iso ./complete
 rm -Rf ./release
 
-livemedia-creator --ks flat-nobara-live-steam-handheld-43.ks --no-virt --resultdir ./release --project Nobara --make-iso --volid Nobara-43 --iso-only --iso-name Nobara-43-Steam-Handheld-$(date +%F).iso --releasever 43 --macboot
+livemedia-creator --ks nv-flat-nobara-live-steam-handheld-44.ks --no-virt --resultdir ./release --project Nobara --make-iso --volid Nobara-44 --iso-only --iso-name Nobara-44-Steam-Handheld-$(date +%F).iso --releasever 44 --macboot --extra-boot-args "modules_load=nvidia" --lorax-templates ./lorax-custom
 mv ./release/*.iso ./complete
 rm -Rf ./release
 
-livemedia-creator --ks nv-flat-nobara-live-official-43.ks --no-virt --resultdir ./release --project Nobara --make-iso --volid Nobara-43 --iso-only --iso-name Nobara-43-Official-NV-$(date +%F).iso --releasever 43 --macboot --extra-boot-args "modules_load=nvidia"
-mv ./release/*.iso ./complete
-rm -Rf ./release
-
-livemedia-creator --ks nv-flat-nobara-live-gnome-43.ks --no-virt --resultdir ./release --project Nobara --make-iso --volid Nobara-43 --iso-only --iso-name Nobara-43-GNOME-NV-$(date +%F).iso --releasever 43 --macboot --extra-boot-args "modules_load=nvidia"
-mv ./release/*.iso ./complete
-rm -Rf ./release
-
-livemedia-creator --ks nv-flat-nobara-live-kde-43.ks --no-virt --resultdir ./release --project Nobara --make-iso --volid Nobara-43 --iso-only --iso-name Nobara-43-KDE-NV-$(date +%F).iso --releasever 43 --macboot --extra-boot-args "modules_load=nvidia"
-mv ./release/*.iso ./complete
-rm -Rf ./release
-
-livemedia-creator --ks nv-flat-nobara-live-steam-htpc-43.ks --no-virt --resultdir ./release --project Nobara --make-iso --volid Nobara-43 --iso-only --iso-name Nobara-43-Steam-HTPC-NV-$(date +%F).iso --releasever 43 --macboot --extra-boot-args "modules_load=nvidia"
-mv ./release/*.iso ./complete
-rm -Rf ./release
 
 ./sha256gen.sh
 chmod -R 777 ./complete
